90bdc360669038e1806c66ff057c519bcd295518
Angel ILIEV <a.v.iliev13@gmail.com>
2018-04-02 11:32:19 +0300

    Support for commemorative coins & ref number matching

--- main.go
+++ main.go
@@ -179,12 +179,6 @@ func ExtractCoinValuesPerPage(url string) []string {
 		// The GetString... fn can fail if the start&end don't occur,
 		// i.e. index -1
 
-		// get value
-		worth := GetStringInBetween(coin_data_arr[i], "class=\"green-11\">", "</a>")
-		if worth == "" {
-			worth = "0.0"
-		}
-
 		// get title
 		ts := GetStringInBetween(coin_data_arr[i], "span class=\"left flag", "</a>")
 		title := strings.Split(ts, "</span>")[1]
@@ -194,6 +188,26 @@ func ExtractCoinValuesPerPage(url string) []string {
 			title = strings.Replace(title, " ", " ", -1)
 		}
 
+		// Get reference number in format of KM# X where X is a number
+		ref_num_str := GetStringInBetween(coin_data_arr[i], "<div class=\"gray-11 km\">", "</div>")
+		ref_num_str_index := strings.Index(ref_num_str, "KM#")
+
+		ref_num := ""
+		// KM# X number was not found -> Extract UC# X number (this is for commemorative coins)
+		if ref_num_str_index == -1 {
+			ref_num_str_index = strings.Index(ref_num_str, "UC#")
+			// If not referefence number is found then assign NULL string to it
+			if ref_num_str_index == -1 {
+				ref_num = "NULL"
+			}
+		}
+
+		// If reference number is found (i.e. not equal to NULL) -> extract it
+		if strings.Compare(ref_num, "NULL") != 0 {
+			// Extracted reference number, either KM# X or UC# X
+			ref_num = ref_num_str[ref_num_str_index:len(ref_num_str)]			
+		}
+		
 		// Check if title has a range of years i.e. "50 fenings 2010-2017"
 		if strings.Contains(title, "-") {
 			// if title is a range of years then the price for each
@@ -203,26 +217,44 @@ func ExtractCoinValuesPerPage(url string) []string {
 			coin_url := GetStringInBetween(coin_data_arr[i], "<div class=\"coin-desc\"><a href=\"", "\" title=")
 			coin_url = "https://en.ucoin.net/" + coin_url
 			coin_list_per_year := ExtractCoinValueForIndividualYear(coin_url, title)
-			coin_list = append(coin_list, coin_list_per_year...)
+
+			// Iterate over coin list per year and add the reference number and an empty/invalid
+			// description (NULL)
+			for _, coin := range coin_list_per_year {
+				coin_with_ref_and_desc := coin + "|" + ref_num + "|" + "NULL"
+				coin_list = append(coin_list, coin_with_ref_and_desc)
+			}
 		} else {
+			// Get coin value
+			worth := GetStringInBetween(coin_data_arr[i], "class=\"green-11\">", "</a>") 
+			// Remove unwanted chars
 			worth = strings.Replace(worth, "$", "", -1)
 			worth = strings.Replace(worth, " ", "", -1)
-
+			// Make sure it is not an empty string and set to 0.0 if it is
 			if strings.Compare(worth, "") == 0 {
 				worth = "0.0"
 			} 
-
+			// Convert value to float
 			coin_value, err := strconv.ParseFloat(worth, 64)
-
+			// Convert value from USD to EUR
 			coin_value_euros := 0.0
 			if err == nil {
 				coin_value_euros = coin_value * USD_EUR_EXCHANGE_RATE	
 			} else {
 				log.Fatalln(err)
 			} 
-			//fmt.Println(coin_value, worth, USD_EUR_EXCHANGE_RATE)
+
+			// Get coin description. Only valid for commemorative coins. It holds information about what
+			// is commemorated i.e. Princess Diana, XXVII Summer Olympic Games Sydney 2000 etc
+			coin_descr_start := "<div class=\"dgray-13\">"
+			coin_descr_end := "</div>"
+			coin_description := ""
+			if strings.Contains(coin_data_arr[i], coin_descr_start) {
+				coin_description = GetStringInBetween(coin_data_arr[i], coin_descr_start, coin_descr_end)
+			}
 			
-			combined_str := title + "|" + strconv.FormatFloat(coin_value_euros, 'f', 2, 64)
+			//Compose a combined coin information string
+			combined_str := title + "|" + strconv.FormatFloat(coin_value_euros, 'f', 2, 64) + "|" + ref_num + "|" + coin_description
 			coin_list = append(coin_list, combined_str)
 			//fmt.Printf("Coin-Title : %s \n Coin-Worth : %s \n\n", title, worth)
 		}
@@ -230,7 +262,7 @@ func ExtractCoinValuesPerPage(url string) []string {
 
 	// fmt.Printf("\nCoin list:\n\n")
 	// for _, item := range coin_list {
-	// 	fmt.Println(item)
+	//  	fmt.Println(item)
 	// }
 
 	return coin_list
@@ -338,6 +370,35 @@ func GetExchangeRateUsdToEuro() float64 {
 	return exchange_rate
 }
 
+func ConvertYearToGregorianCalendar(coin_country, coin_year string) string {
+	// Sometimes the conversion of islamic years is not 100% accurate
+	// so it might be needed to send a range of years
+	// i.e if the computation results in 1393 -> 1972.78 => send back 1972-1973
+	// Convertion formula: ( ( 32 x islamic_years ) / 33 ) + 622
+	islam_countries := [17]string{
+		"Iran", "Libya", "Morocco", "Sudan", "Kuwait",
+		"Egypt", "Tunisia", "Afghanistan", "Yemen",
+		"Syria", "Iraq", "United Arab Emirates", "Saudi Arabia",
+		"Jordan", "Qatar", "Oman", "Bahrain",
+	}
+
+	// Japanese years correspond to the number of years the current emperor has ruled
+	// Therefore to find the current year, add the starting year of the rule
+	// to the current year (-1 because he ruled in the 0th year)
+	// Taisho 14 -> Taisho 1 (1912) + 14 - 1 = 1925
+	// Starting years: Taisho (1912), Showa (1926), Heisei (1989)
+	japanese_years := [3]string{
+		"Taisho", "Showa", "Heisei",
+	}
+
+	// Taiwan starts counting years from the founding of
+	// the Republic of China i.e. 1912 is year 1
+	// In order to find the current year: 1912 + taiwan year - 1 = current year
+	taiwan_start_year := 1912
+	
+	return "year-goes-here"
+}
+
 func MatchCoinsAndWriteToExcel(filename, country string, coins []string) {
 	excelFileName := filename
 
@@ -352,8 +413,12 @@ func MatchCoinsAndWriteToExcel(filename, country string, coins []string) {
 			// fmt.Println(row)
 			coin_data := row.Cells
 			coin_ctry := coin_data[0].String() // Col A
+			coin_rf := coin_data[1].String() // Col B
 			coin_ttle := coin_data[3].String() // Col D
 			coin_yr := coin_data[4].String() // Col E
+			// If coin country is islamic and year is <1500 -> convert to Gregorian Calendar
+			// If coin country is Japan, Taiwan and year is <200 -> convert to Gregorian Calendar 
+			//coin_yr = ConvertYearToGregorianCalendar(coin_ctry, coin_yr)
 			coin_i := coin_ttle + " " + coin_yr 
 
 			// Technically both country strings should match perfectly since they
@@ -364,6 +429,7 @@ func MatchCoinsAndWriteToExcel(filename, country string, coins []string) {
 			country_match_score := similartxt.SimilarText(coin_ctry, country)
 			coin_best_match := 0
 			coin_value_best_match := ""
+			coin_descrp_best_match := ""
 			coin_info := ""
 			coin_match := ""
 			
@@ -372,7 +438,9 @@ func MatchCoinsAndWriteToExcel(filename, country string, coins []string) {
 					coin_arr := strings.Split(coin, "|")
 					coin_info = coin_arr[0]
 					coin_value := coin_arr[1]
-
+					coin_ref_num := coin_arr[2]
+					coin_description := coin_arr[3]
+					
 					// Extracts coin nominal string with the trailing SPACE
 					ext_coin_nominal := strings.Split(coin_info, " ")[0] + " " 
 					coin_nominal := strings.Split(coin_ttle, " ")[0] + " "
@@ -387,22 +455,63 @@ func MatchCoinsAndWriteToExcel(filename, country string, coins []string) {
 						for word_c := 0; word_c < len(coin_info_arr) - 1; word_c++ {
 							ext_coin_title = ext_coin_title + coin_info_arr[word_c] 
 						}
-						
-						if strings.Compare(coin_year, coin_yr) == 0 {
-							coin_match_score := similartxt.SimilarText(coin_ttle, ext_coin_title)
 
-							if coin_match_score > coin_best_match {
-								coin_value_best_match = coin_value
-								coin_best_match = coin_match_score
-								coin_match = coin_info
-							}
+						if strings.Compare(coin_year, coin_yr) == 0 {
+							// Check if the coin in question is a commemorative coin
+							// that is if it has a description inside ()
+							if strings.Contains(coin_ttle, "(") /* && len(strings.Split(coin_ttle, " ")) > 1 */ {
+								// if the coin that we are trying to match to the excel
+								// also has a description (i.e. commemorative information)
+								if strings.Compare(coin_description, "NULL") != 0 {
+									// Create full title+(description) for extracted coin
+									// and compare with excel title (which already has the same format)
+									ext_coin_ttle_full := ext_coin_title + "(" + coin_description + ")"
+									coin_match_score := similartxt.SimilarText(coin_ttle, ext_coin_ttle_full)
+									//fmt.Printf("Trying to match (%d): %s *WITH* %s\n", coin_match_score, coin_ttle, ext_coin_ttle_full) 
+									
+									if coin_match_score >= coin_best_match {
+										coin_value_best_match = coin_value
+										coin_best_match = coin_match_score
+										coin_match = coin_info
+										coin_descrp_best_match = coin_description
+									}
+								} else {
+									// skip coins that don't have the commemorative descrp
+									continue
+								}	
+							} else {
+								//fmt.Printf("Trying to compare: %s -> %s \n", coin_rf, coin_ref_num)
+								// its not a commemorative coin -> compare reference numbers (if they are not NULL or empty)
+								if strings.Contains(coin_rf, "KM#") && !strings.Contains(coin_ref_num, "NULL") {
+									if strings.Compare(coin_rf, coin_ref_num) == 0 {
+										coin_match_score := similartxt.SimilarText(coin_ttle, ext_coin_title)
+
+										if coin_match_score >= coin_best_match {
+											coin_value_best_match = coin_value
+											coin_best_match = coin_match_score
+											coin_match = coin_info
+											coin_descrp_best_match = coin_description
+										}
+									}
+								} else {
+									// No reference number is present -> compare just titles
+									coin_match_score := similartxt.SimilarText(coin_ttle, ext_coin_title)
+
+									if coin_match_score >= coin_best_match {
+										coin_value_best_match = coin_value
+										coin_best_match = coin_match_score
+										coin_match = coin_info
+										coin_descrp_best_match = coin_description
+									} 
+								} 
+							}							
 						}
 					}
 				}
 				if coin_best_match == 0 {
 					fmt.Printf("M(0): %s -> No match was found\n", coin_i)
 				} else {
-					fmt.Printf("M(%d): %s -> %s | Value: %s \n", coin_best_match, coin_match, coin_i, coin_value_best_match)
+					fmt.Printf("M(%d): %s -> %s | Value: %s | Commem: %s\n", coin_best_match, coin_i, coin_match, coin_value_best_match, coin_descrp_best_match)
 				}
 			}			
 		}
@@ -410,11 +519,6 @@ func MatchCoinsAndWriteToExcel(filename, country string, coins []string) {
 }
 
 func main() {
-	//ExtractCoinValuesForCountry("bhutan")
-
-	//ReadExcelDoc("/home/angel/go/CoinAvg/coin_sample.xlsx")
-	//ReadExcelDoc("/home/angel/go/CoinAvg/tauras651_coins.xlsx")
-	//ReadExcelDoc("C:\\Users\\angel.iliev\\go\\coin-avg-master\\tauras651_coins.xlsx")
 	//excel_filename := "C:\\Users\\angel.iliev\\go\\coin-avg-master\\coin_sample.xlsx"
 	excel_filename := "/home/angel/go/CoinAvg/coin_sample.xlsx"
 	country_list := ReadExcelDoc(excel_filename)
@@ -431,15 +535,6 @@ func main() {
 		MatchCoinsAndWriteToExcel(excel_filename, country, coin_list) 
 	}
 
-	//test := similartxt.SimilarText("Hello World!", "Hello, my beautiful world!")
-	//fmt.Println(test)
-	// TODO: properly decode strings - now showing weird chars
-	// TODO: implement iteration over all pages for a given country 
-
-	// -STEP 1: generate a list of all countries from excel
-	// -STEP 2: convert the country string to expected format
-	// -STEP 3: iterate over country list and obtain coin info
-	// STEP 4: for every coin match an entry from excel & write value
 	//   STEP 4.1: if the best match is low (<50%) add a comment
 	//             with the original title it was matched to
 	//             In this way you can examine manually if the
@@ -449,12 +544,5 @@ func main() {
 	//       Be careful when writing to the file so as not to
 	//       Overwrite existing data
 
-	// NOTE1: First match nominal, then match year, only after that match coin!!!!!!
-	
-	// TIP: instead of constantly reading the excel file
-	//      create a coin struct that will contain all info
-	//      + the index (row) of the coin in question
-	//      In this way, you can iterate over the already
-	//      loaded array of coins and we don't lose time
-	//      constantly reading and writing from/to file
+	// TODO: Add year conversion for islamic countries, japan, taiwan etc.
 }
